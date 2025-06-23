3deb9bebb742c56feb7111712ce77e0d01df2681
Quinn Daley <quinn@fishpercolator.co.uk>
2025-06-23 17:03:09 +0100

    Add RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://cerulean-sfogliatella-b90182.netlify.app/"
+});
