508296bf7d2b5f6e35e847a769229b13eb40465b
Quinn Daley <quinn@fishpercolator.co.uk>
2025-06-23 17:12:44 +0100

    Add a React-based greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cerulean-sfogliatella-b90182.netlify.app/"
+  site: "https://cerulean-sfogliatella-b90182.netlify.app/",
+  integrations: [preact()]
 });
